99539b6cb7ac5ac2b7701da09c948aa7113cb008
Ge Gao <gaoge@fb.com>
2021-04-05 14:01:32 -0700

    Include all tables in the column mapping of MVs
    Support the computation of 1-to-N column mappings from the view
    to the base table columns for materialized views. Matched base
    table columns are derived by parsing equi join and set operation.
    
    The computation logic is put into MaterializedViewPlanValidator
    to avoid logic duplication. The assumption is a valid materialized
    view plan means we are able to derive the right column mappings
    for SELECT and REFRESH.

--- presto-main/src/main/java/com/facebook/presto/sql/analyzer/MaterializedViewPlanValidator.java
+++ presto-main/src/main/java/com/facebook/presto/sql/analyzer/MaterializedViewPlanValidator.java
@@ -13,123 +13,231 @@
  */
 package com.facebook.presto.sql.analyzer;
 
+import com.facebook.presto.spi.SchemaTableName;
 import com.facebook.presto.sql.tree.AliasedRelation;
 import com.facebook.presto.sql.tree.ComparisonExpression;
 import com.facebook.presto.sql.tree.DefaultTraversalVisitor;
 import com.facebook.presto.sql.tree.Join;
 import com.facebook.presto.sql.tree.JoinCriteria;
 import com.facebook.presto.sql.tree.JoinOn;
-import com.facebook.presto.sql.tree.JoinUsing;
 import com.facebook.presto.sql.tree.LogicalBinaryExpression;
-import com.facebook.presto.sql.tree.Node;
+import com.facebook.presto.sql.tree.Query;
+import com.facebook.presto.sql.tree.SetOperation;
 import com.facebook.presto.sql.tree.Unnest;
+import com.google.common.collect.ImmutableList;
 
-import java.util.HashSet;
-import java.util.Set;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Optional;
 
+import static com.facebook.presto.metadata.MetadataUtil.toSchemaTableName;
+import static com.facebook.presto.spi.ConnectorMaterializedViewDefinition.TableColumn;
 import static com.facebook.presto.sql.analyzer.SemanticErrorCode.NOT_SUPPORTED;
+import static com.google.common.base.Preconditions.checkArgument;
+import static com.google.common.collect.ImmutableList.toImmutableList;
 import static java.util.Objects.requireNonNull;
 
 // TODO: Add more cases https://github.com/prestodb/presto/issues/16032
 public class MaterializedViewPlanValidator
-        extends DefaultTraversalVisitor<Void, MaterializedViewPlanValidator.MaterializedViewPlanValidatorContext>
 {
-    private final Node query;
+    private Query viewQuery;
 
-    public MaterializedViewPlanValidator(Node query)
+    public MaterializedViewPlanValidator(Query viewQuery)
     {
-        this.query = requireNonNull(query, "query is null");
+        this.viewQuery = requireNonNull(viewQuery, "viewQuery is null");
     }
 
-    @Override
-    protected Void visitJoin(Join node, MaterializedViewPlanValidatorContext context)
+    public void validate()
     {
-        context.getJoinNodes().add(node);
-        if (context.getJoinNodes().size() > 1) {
-            throw new SemanticException(NOT_SUPPORTED, query, "More than one join in materialized view is not supported yet.");
-        }
-
-        switch (node.getType()) {
-            case INNER:
-                if (!node.getType().equals(Join.Type.INNER)) {
-                    throw new SemanticException(NOT_SUPPORTED, node, "Only inner join is supported for materialized view.");
-                }
-                if (!node.getCriteria().isPresent()) {
-                    throw new SemanticException(NOT_SUPPORTED, node, "Join with no criteria is not supported for materialized view.");
-                }
-
-                JoinCriteria joinCriteria = node.getCriteria().get();
-                if (!(joinCriteria instanceof JoinOn) && !(joinCriteria instanceof JoinUsing)) {
-                    throw new SemanticException(NOT_SUPPORTED, node, "Only join-on and join-using are supported for materialized view.");
-                }
-
-                context.setProcessingJoinNode(true);
-                if (joinCriteria instanceof JoinOn) {
-                    process(((JoinOn) joinCriteria).getExpression(), context);
-                }
-                context.setProcessingJoinNode(false);
-                break;
-            case CROSS:
-                if (!(node.getRight() instanceof AliasedRelation)) {
-                    throw new SemanticException(NOT_SUPPORTED, node, "Only cross join with unnest is supported for materialized view.");
-                }
-                AliasedRelation right = (AliasedRelation) node.getRight();
-                if (!(right.getRelation() instanceof Unnest)) {
-                    throw new SemanticException(NOT_SUPPORTED, node, "Only cross join with unnest is supported for materialized view.");
-                }
-
-                process(node.getLeft(), context);
-
-                break;
-
-            default:
-                throw new SemanticException(NOT_SUPPORTED, node, "Only inner join is supported for materialized view.");
-        }
-
-        return null;
+        new Visitor().process(viewQuery, new Context());
     }
 
-    @Override
-    protected Void visitLogicalBinaryExpression(LogicalBinaryExpression node, MaterializedViewPlanValidatorContext context)
+    public List<List<TableColumn>> getLinkedBaseColumns(Analysis analysis)
     {
-        if (!context.isProcessingJoinNode()) {
+        Context context = new Context();
+        new Visitor(true, Optional.of(analysis)).process(viewQuery, context);
+        return context.getLinkedBaseColumns();
+    }
+
+    private static class Visitor
+            extends DefaultTraversalVisitor<Void, Context>
+    {
+        private final boolean analyzeColumnMapping;
+        private final Optional<Analysis> analysis;
+
+        public Visitor()
+        {
+            this(false, Optional.empty());
+        }
+
+        public Visitor(boolean analyzeColumnMapping, Optional<Analysis> analysis)
+        {
+            checkArgument(!analyzeColumnMapping || analysis.isPresent(), "analysis must be set if column mapping is to be computed.");
+
+            this.analyzeColumnMapping = analyzeColumnMapping;
+            this.analysis = requireNonNull(analysis, "analysis is null");
+        }
+
+        @Override
+        protected Void visitJoin(Join node, Context context)
+        {
+            context.pushJoinNode(node);
+
+            if (context.getJoinNodes().size() > 1) {
+                throw new SemanticException(NOT_SUPPORTED, node, "More than one join in materialized view is not supported yet.");
+            }
+
+            switch (node.getType()) {
+                case INNER:
+                    if (!node.getCriteria().isPresent()) {
+                        throw new SemanticException(NOT_SUPPORTED, node, "Inner join with no criteria is not supported for materialized view.");
+                    }
+
+                    JoinCriteria joinCriteria = node.getCriteria().get();
+                    if (!(joinCriteria instanceof JoinOn)) {
+                        throw new SemanticException(NOT_SUPPORTED, node, "Only join-on is supported for materialized view.");
+                    }
+
+                    process(node.getLeft(), context);
+                    process(node.getRight(), context);
+
+                    context.setProcessingJoinNode(true);
+                    if (joinCriteria instanceof JoinOn) {
+                        process(((JoinOn) joinCriteria).getExpression(), context);
+                    }
+                    context.setProcessingJoinNode(false);
+
+                    break;
+                case CROSS:
+                    if (!(node.getRight() instanceof AliasedRelation)) {
+                        throw new SemanticException(NOT_SUPPORTED, node, "Cross join is supported only with unnest for materialized view.");
+                    }
+                    AliasedRelation right = (AliasedRelation) node.getRight();
+                    if (!(right.getRelation() instanceof Unnest)) {
+                        throw new SemanticException(NOT_SUPPORTED, node, "Cross join is supported only with unnest for materialized view.");
+                    }
+
+                    process(node.getLeft(), context);
+
+                    break;
+
+                default:
+                    throw new SemanticException(NOT_SUPPORTED, node, "Only inner join and cross join unnested are supported for materialized view.");
+            }
+
+            context.popJoinNode();
             return null;
         }
 
-        // TODO: It should only support equi join case https://github.com/prestodb/presto/issues/16033
-        if (!node.getOperator().equals(LogicalBinaryExpression.Operator.AND)) {
-            throw new SemanticException(NOT_SUPPORTED, node, "Only AND operator is supported for join criteria for materialized view.");
-        }
-        return super.visitLogicalBinaryExpression(node, context);
-    }
+        @Override
+        protected Void visitSetOperation(SetOperation node, Context context)
+        {
+            if (analyzeColumnMapping) {
+                analyzeSetOperatorForLinkedBaseColumns(node, context);
+            }
 
-    @Override
-    protected Void visitComparisonExpression(ComparisonExpression node, MaterializedViewPlanValidatorContext context)
-    {
-        if (!context.isProcessingJoinNode()) {
+            return super.visitSetOperation(node, context);
+        }
+
+        @Override
+        protected Void visitLogicalBinaryExpression(LogicalBinaryExpression node, Context context)
+        {
+            if (context.isProcessingJoinNode()) {
+                if (!node.getOperator().equals(LogicalBinaryExpression.Operator.AND)) {
+                    throw new SemanticException(NOT_SUPPORTED, node, "Only AND operator is supported for join criteria for materialized view.");
+                }
+            }
+
+            return super.visitLogicalBinaryExpression(node, context);
+        }
+
+        @Override
+        protected Void visitComparisonExpression(ComparisonExpression node, Context context)
+        {
+            if (context.isProcessingJoinNode()) {
+                if (!node.getOperator().equals(ComparisonExpression.Operator.EQUAL)) {
+                    throw new SemanticException(NOT_SUPPORTED, node, "Only EQUAL join is supported for materialized view.");
+                }
+
+                if (analyzeColumnMapping) {
+                    analyzeComparisonExpressionForLinkedBaseColumns(node, context);
+                }
+            }
+
             return super.visitComparisonExpression(node, context);
         }
 
-        if (!node.getOperator().equals(ComparisonExpression.Operator.EQUAL)) {
-            throw new SemanticException(NOT_SUPPORTED, node, "Only EQUAL join is supported for materialized view.");
+        private void analyzeSetOperatorForLinkedBaseColumns(SetOperation node, Context context)
+        {
+            List<RelationType> outputDescriptorList = node.getRelations().stream()
+                    .map(relation -> analysis.get().getOutputDescriptor(relation).withOnlyVisibleFields())
+                    .collect(toImmutableList());
+
+            int numRelations = outputDescriptorList.size();
+            int numFields = outputDescriptorList.get(0).getVisibleFieldCount();
+            for (int fieldIndex = 0; fieldIndex < numFields; fieldIndex++) {
+                for (int firstRelationIndex = 0; firstRelationIndex < numRelations; firstRelationIndex++) {
+                    Optional<TableColumn> firstBaseColumn = tryGetOriginalTableColumn(outputDescriptorList.get(firstRelationIndex).getFieldByIndex(fieldIndex));
+                    for (int secondRelationIndex = firstRelationIndex + 1; secondRelationIndex < numRelations; secondRelationIndex++) {
+                        Optional<TableColumn> secondBaseColumn = tryGetOriginalTableColumn(outputDescriptorList.get(secondRelationIndex).getFieldByIndex(fieldIndex));
+                        if (firstBaseColumn.isPresent() && secondBaseColumn.isPresent() && !firstBaseColumn.get().equals(secondBaseColumn.get())) {
+                            context.addLinkedBaseColumns(firstBaseColumn.get(), secondBaseColumn.get());
+                        }
+                    }
+                }
+            }
         }
 
-        return super.visitComparisonExpression(node, context);
+        private void analyzeComparisonExpressionForLinkedBaseColumns(ComparisonExpression node, Context context)
+        {
+            Field left = analysis.get().getScope(context.getTopJoinNode()).tryResolveField(node.getLeft())
+                    .orElseThrow(() -> new SemanticException(
+                            NOT_SUPPORTED,
+                            node.getLeft(),
+                            "%s in join criteria is not supported for materialized view.", node.getLeft().getClass().getSimpleName()))
+                    .getField();
+            Field right = analysis.get().getScope(context.getTopJoinNode()).tryResolveField(node.getRight())
+                    .orElseThrow(() -> new SemanticException(
+                            NOT_SUPPORTED,
+                            node.getRight(),
+                            "%s in join criteria is not supported for materialized view.", node.getRight().getClass().getSimpleName()))
+                    .getField();
+
+            Optional<TableColumn> leftBaseColumn = tryGetOriginalTableColumn(left);
+            Optional<TableColumn> rightBaseColumn = tryGetOriginalTableColumn(right);
+            if (leftBaseColumn.isPresent() && rightBaseColumn.isPresent() && !leftBaseColumn.get().equals(rightBaseColumn.get())) {
+                context.addLinkedBaseColumns(leftBaseColumn.get(), rightBaseColumn.get());
+            }
+        }
+
+        private static Optional<TableColumn> tryGetOriginalTableColumn(Field field)
+        {
+            if (field.getOriginTable().isPresent() && field.getOriginColumnName().isPresent()) {
+                SchemaTableName table = toSchemaTableName(field.getOriginTable().get());
+                String column = field.getOriginColumnName().get();
+                return Optional.of(new TableColumn(table, column));
+            }
+            return Optional.empty();
+        }
     }
 
-    protected static final class MaterializedViewPlanValidatorContext
+    private static final class Context
     {
-        private final Set<Join> joinNodes;
         private boolean isProcessingJoinNode;
+        private final LinkedList<Join> joinNodeStack;
 
-        public MaterializedViewPlanValidatorContext()
+        private ImmutableList.Builder<List<TableColumn>> linkedBaseColumns;
+
+        public Context()
         {
-            joinNodes = new HashSet<>();
+            isProcessingJoinNode = false;
+            joinNodeStack = new LinkedList<>();
+            linkedBaseColumns = ImmutableList.builder();
         }
 
-        public Set<Join> getJoinNodes()
+        public boolean isProcessingJoinNode()
         {
-            return joinNodes;
+            return isProcessingJoinNode;
         }
 
         public void setProcessingJoinNode(boolean processingJoinNode)
@@ -137,9 +245,34 @@ public class MaterializedViewPlanValidator
             isProcessingJoinNode = processingJoinNode;
         }
 
-        public boolean isProcessingJoinNode()
+        public void pushJoinNode(Join join)
         {
-            return isProcessingJoinNode;
+            joinNodeStack.push(join);
+        }
+
+        public Join popJoinNode()
+        {
+            return joinNodeStack.pop();
+        }
+
+        public Join getTopJoinNode()
+        {
+            return joinNodeStack.getFirst();
+        }
+
+        public List<Join> getJoinNodes()
+        {
+            return ImmutableList.copyOf(joinNodeStack);
+        }
+
+        public void addLinkedBaseColumns(TableColumn first, TableColumn second)
+        {
+            linkedBaseColumns.add(ImmutableList.of(first, second));
+        }
+
+        public List<List<TableColumn>> getLinkedBaseColumns()
+        {
+            return linkedBaseColumns.build();
         }
     }
 }
--- presto-main/src/main/java/com/facebook/presto/sql/analyzer/StatementAnalyzer.java
+++ presto-main/src/main/java/com/facebook/presto/sql/analyzer/StatementAnalyzer.java
@@ -191,6 +191,7 @@ import static com.facebook.presto.spi.StandardWarningCode.PERFORMANCE_WARNING;
 import static com.facebook.presto.spi.StandardWarningCode.REDUNDANT_ORDER_BY;
 import static com.facebook.presto.spi.function.FunctionKind.AGGREGATE;
 import static com.facebook.presto.spi.function.FunctionKind.WINDOW;
+import static com.facebook.presto.sql.MaterializedViewUtils.validateMaterializedViewQuery;
 import static com.facebook.presto.sql.NodeUtils.getSortItemsFromOrderBy;
 import static com.facebook.presto.sql.NodeUtils.mapFromProperties;
 import static com.facebook.presto.sql.ParsingUtil.createParsingOptions;
@@ -206,7 +207,6 @@ import static com.facebook.presto.sql.analyzer.ExpressionAnalyzer.getExpressionT
 import static com.facebook.presto.sql.analyzer.ExpressionTreeUtils.extractAggregateFunctions;
 import static com.facebook.presto.sql.analyzer.ExpressionTreeUtils.extractExpressions;
 import static com.facebook.presto.sql.analyzer.ExpressionTreeUtils.extractWindowFunctions;
-import static com.facebook.presto.sql.analyzer.MaterializedViewPlanValidator.MaterializedViewPlanValidatorContext;
 import static com.facebook.presto.sql.analyzer.PredicateStitcher.PredicateStitcherContext;
 import static com.facebook.presto.sql.analyzer.RefreshMaterializedViewPredicateAnalyzer.extractTablePredicates;
 import static com.facebook.presto.sql.analyzer.ScopeReferenceExtractor.hasReferencesToScope;
@@ -683,7 +683,6 @@ class StatementAnalyzer
                 }
                 throw new SemanticException(MATERIALIZED_VIEW_ALREADY_EXISTS, node, "Destination materialized view '%s' already exists", viewName);
             }
-            validateMaterialziedViewQueryPlan(node.getQuery());
             validateProperties(node.getProperties(), scope);
 
             analysis.setCreateTableProperties(mapFromProperties(node.getProperties()));
@@ -700,12 +699,6 @@ class StatementAnalyzer
             validateBaseTables(analysis.getTableNodes(), node);
 
             return createAndAssignScope(node, scope);
-        }
-
-        private void validateMaterialziedViewQueryPlan(Statement query)
-        {
-            MaterializedViewPlanValidator validator = new MaterializedViewPlanValidator(query);
-            validator.process(query, new MaterializedViewPlanValidatorContext());
         }
 
         @Override
@@ -1326,7 +1319,7 @@ class StatementAnalyzer
                 ConnectorMaterializedViewDefinition materializedViewDefinition,
                 MaterializedViewStatus materializedViewStatus)
         {
-            validateMaterialziedViewQueryPlan(sqlParser.createStatement(materializedViewDefinition.getOriginalSql(), createParsingOptions(session, warningCollector)));
+            validateMaterializedViewQuery((Query) sqlParser.createStatement(materializedViewDefinition.getOriginalSql(), createParsingOptions(session, warningCollector)));
 
             String newSql = getMaterializedViewSQL(materializedView, materializedViewDefinition, materializedViewStatus);
 
